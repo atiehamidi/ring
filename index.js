7d8549d66c0d9030cceb0a7479a61f511e895e4b
Atieh Hamidi <atieh@MacBook-Pro.local>
2020-06-25 16:45:33 +0200

    use npm and git ignore for this project

---
+++ index.js
@@ -0,0 +1,7 @@
+const toTitleCase = require("to-title-case")
+
+console.log("toTitleCase;", toTitleCase)
+const lower = 'the lord of the rings: the return of the king'
+
+const title = toTitleCase(lower)
+console.log(title)
